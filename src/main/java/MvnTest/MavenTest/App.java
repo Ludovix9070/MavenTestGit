ec5ddb2ecff5e592d87b0f72a1063a5698b3480a
Ludovix9070 <ludovix9070@gmail.com>
2022-03-28 16:49:18 +0200

    Fourth commit

--- src/main/java/MvnTest/MavenTest/App.java
+++ src/main/java/MvnTest/MavenTest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Third version" );
+        System.out.println( "Hello World! Fourth version" );
     }
 }
