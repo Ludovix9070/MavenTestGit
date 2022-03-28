7bbce65774cc9173c4890dbb7fa060ba19ea763a
Ludovix9070 <ludovix9070@gmail.com>
2022-03-28 16:18:33 +0200

    Third Commit

--- src/main/java/MvnTest/MavenTest/App.java
+++ src/main/java/MvnTest/MavenTest/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! Second version" );
+        System.out.println( "Hello World! Third version" );
     }
 }
