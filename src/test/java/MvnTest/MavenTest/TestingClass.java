ecd15163b234fb4ea2e83da3ddbe6ec442fe9b77
Ludovix9070 <ludovix9070@gmail.com>
2022-03-28 12:58:53 +0200

    First Commit

---
+++ src/test/java/MvnTest/MavenTest/TestingClass.java
@@ -0,0 +1,15 @@
+package MvnTest.MavenTest;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestingClass {
+
+	@Test
+	public void test() {
+		assertTrue(true);
+		
+	}
+
+}
